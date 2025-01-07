7ce33aa2f94713a10ae5546728ea59d343285d35
bjing <bjing2016@github.com>
2025-01-07 08:49:29 -0600

    codesign

---
+++ openprot/evals/codesign.py
@@ -0,0 +1,142 @@
+from .eval import OpenProtEval
+# import foldcomp
+from ..utils import protein
+from ..utils.prot_utils import make_ca_prot, write_ca_traj
+from ..utils.geometry import compute_lddt, rmsdalign, compute_rmsd
+from ..utils import residue_constants as rc
+from ..generate.sampler import OpenProtSampler
+from ..generate.structure import EDMDiffusionStepper
+from ..generate.sequence import SequenceUnmaskingStepper
+import numpy as np
+from ..tasks import StructurePrediction
+import torch
+import os, tqdm, math, subprocess
+import pandas as pd
+
+
+class CodesignEval(OpenProtEval):
+    def setup(self):
+        pass
+
+    def run(self, model):
+        NotImplemented
+
+    def __len__(self):
+        return self.cfg.num_samples
+
+    def __getitem__(self, idx):
+        L = self.cfg.sample_length
+        data = self.make_data(
+            name=f"sample{idx}",
+            seqres="A" * L,
+            seq_mask=np.ones(L, dtype=np.float32),
+            seq_noise=np.ones(L, dtype=np.float32),
+            struct_noise=np.ones(L, dtype=np.float32) * self.cfg.sigma_max,
+            atom37=np.zeros((L, 37, 3), dtype=np.float32),
+            atom37_mask=np.ones((L, 37), dtype=np.float32),
+        )
+        return data
+
+    def compute_metrics(
+        self, rank=0, world_size=1, device=None, savedir=".", logger=None
+    ):
+
+        if self.cfg.run_designability:
+            torch.cuda.empty_cache()
+
+            idx = list(range(rank, len(self), world_size))
+            os.makedirs(f"{savedir}/rank{rank}", exist_ok=True)
+            for i in idx:
+                cmd = ['cp', f"{savedir}/sample{i}.fasta", f"{savedir}/rank{rank}"]
+                subprocess.run(cmd)
+                
+            cmd = [
+                "bash",
+                "scripts/switch_conda_env.sh",
+                "python",
+                "-m",
+                "scripts.esmfold",
+                "--outdir",
+                savedir,
+                "--dir",
+                f"{savedir}/rank{rank}",
+                # "--print",
+            ]
+            
+            out = subprocess.run(cmd)  
+            
+            for i in idx:
+                
+                with open(f"{savedir}/sample{i}.pdb") as f:
+                    prot = protein.from_pdb_string(f.read())
+                with open(f"{savedir}/sample{i}.pdb") as f:
+                    pred = protein.from_pdb_string(f.read())
+                lddt = compute_lddt(
+                    torch.from_numpy(pred.atom_positions[:,1]), 
+                    torch.from_numpy(prot.atom_positions[:,1]), 
+                    torch.from_numpy(prot.atom_mask[:,1])
+                )
+                rmsd = compute_rmsd(
+                    torch.from_numpy(pred.atom_positions[:,1]),  
+                    torch.from_numpy(prot.atom_positions[:,1])
+                )
+                if logger is not None:
+                    logger.log(f"{self.cfg.name}/sclddt", lddt)
+                    logger.log(f"{self.cfg.name}/scrmsd", rmsd)
+                    logger.log(f"{self.cfg.name}/scrmsd<2", (rmsd < 2).float())
+                
+
+    def run_batch(
+        self,
+        model,
+        batch: dict,
+        noisy_batch: dict,
+        savedir=".", 
+        device=None,
+        logger=None
+    ):
+
+        def sched_fn(t):
+            p = self.cfg.sched_p
+            return (
+                self.cfg.sigma_min ** (1 / p)
+                + (1-t) * (self.cfg.sigma_max ** (1 / p) - self.cfg.sigma_min ** (1 / p))
+            ) ** p
+
+        sampler = OpenProtSampler(schedules={
+            'structure': sched_fn,
+            'sequence': lambda t: 1-t
+        }, steppers=[
+            EDMDiffusionStepper(self.cfg.struct),
+            SequenceUnmaskingStepper(self.cfg.seq)
+        ])
+        
+        sample, extra = sampler.sample(model, noisy_batch, self.cfg.steps)
+
+        pred_traj = torch.stack(extra['preds'])
+        samp_traj = torch.stack(extra['traj'])
+
+        B = len(sample['struct'])
+        
+        for i in range(B):
+            prot = make_ca_prot(
+                sample['struct'][i].cpu().numpy(),
+                batch["aatype"][i].cpu().numpy(),
+                batch["struct_mask"][i].cpu().numpy(),
+            )
+    
+            ref_str = protein.to_pdb(prot)
+            name = batch["name"][i]
+            with open(f"{savedir}/{name}.pdb", "w") as f:
+                f.write(ref_str)
+    
+            with open(f"{savedir}/{name}_traj.pdb", "w") as f:
+                f.write(write_ca_traj(prot, samp_traj[:, i].cpu().numpy()))
+    
+            with open(f"{savedir}/{name}_pred_traj.pdb", "w") as f:
+                f.write(write_ca_traj(prot, pred_traj[:, i].cpu().numpy()))
+
+            seq = "".join([rc.restypes_with_x[aa] for aa in sample["aatype"][i]])
+            with open(f"{savedir}/{name}.fasta", "w") as f:
+                f.write(f">{name}\n")  # FASTA format header
+                f.write(seq + "\n")
---
+++ openprot/tasks/codesign.py
@@ -0,0 +1,68 @@
+from .task import OpenProtTask
+import numpy as np
+from ..utils import residue_constants as rc
+from scipy.spatial.transform import Rotation as R
+
+
+class Codesign(OpenProtTask):
+
+    def register_loss_masks(self):
+        return ["/codesign"]
+
+    def prep_data(self, data, crop=None, eps=1e-6):
+
+        if crop is not None:
+            data.crop(crop)
+
+        data["/codesign"] = np.ones((), dtype=np.float32)
+
+    def add_sequence_noise(self, data):
+        rand = np.random.rand()
+        if rand < self.cfg.seq_max_noise_prob:
+            noise_level = 1.0
+        elif rand < self.cfg.seq_max_noise_prob + self.cfg.seq_uniform_prob:
+            noise_level = np.random.rand()
+        else:
+            noise_level = np.random.beta(*self.cfg.seq_beta)
+
+        L = len(data["seqres"])
+        data["seq_noise"] = (np.random.rand(L) < noise_level).astype(np.float32)
+        t_inv = data["seq_mask"].sum() / (eps + (data["seq_mask"] * data["seq_noise"]).sum()) 
+        t = (data["seq_mask"] * data["seq_noise"]).sum()  / (eps + data["seq_mask"].sum()) 
+        data["seq_weight"] = np.ones(L, dtype=np.float32) * (1-noise_level) * self.cfg.seq_weight
+        
+        
+    def add_structure_noise(self, data):
+        rand = np.random.rand()
+        if rand < self.cfg.struct_uniform_prob:
+            noise_level = np.random.rand()
+        elif rand < self.cfg.struct_uniform_prob + self.cfg.cfg.struct_uniform_prob:
+            noise_level = 1.0
+        else:
+            noise_level = np.random.beta(*self.cfg.struct_beta)
+
+        L = len(data["seqres"])
+        
+        ##############
+
+        p = self.cfg.sched_p
+        noise_level = (
+            self.cfg.sigma_min ** (1 / p)
+            + noise_level * (self.cfg.sigma_max ** (1 / p) - self.cfg.sigma_min ** (1 / p))
+        ) ** p
+        #####
+        
+        data["struct_noise"] = np.ones(L, dtype=np.float32) * noise_level
+        data["struct_weight"] = np.ones(L, dtype=np.float32) * self.cfg.struct_weight
+
+        # center the structures
+        pos = data["atom37"][..., rc.atom_order["CA"], :]
+        mask = data["atom37_mask"][..., rc.atom_order["CA"], None]
+        com = (pos * mask).sum(-2) / (mask.sum(-2) + eps)
+        data["atom37"] -= com
+
+        if self.cfg.random_rot:
+            randrot = R.random().as_matrix()
+            data["atom37"] @= randrot.T
+
+    
