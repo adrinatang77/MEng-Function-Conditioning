7433d9ef46177dd601d5f053462d3fd1d600cf4e
bjing2016 <bjing2016@github.com>
2025-02-18 09:50:03 -0500

    struct pred diff

--- openprot/evals/struct_pred.py
+++ openprot/evals/struct_pred.py
@@ -4,6 +4,8 @@ from ..utils import protein
 from ..utils.geometry import compute_lddt, rmsdalign, compute_rmsd
 from ..utils import residue_constants as rc
 from ..utils.prot_utils import make_ca_prot, compute_tmscore, write_ca_traj
+from ..generate.sampler import OpenProtSampler
+from ..generate.structure import EDMDiffusionStepper, GaussianFMStepper
 import numpy as np
 import torch
 import os, tqdm
@@ -40,32 +42,51 @@ class StructurePredictionEval(OpenProtEval):
         return data
 
     def run_diffusion(self, model, batch, noisy_batch, savedir):
-        diffusion = self.tracks["StructureTrack"].diffusion
+        def edm_sched_fn(t):
+            p = self.cfg.struct.sched_p
+            sigma_max = self.cfg.struct.sigma_max
+            sigma_min = self.cfg.struct.sigma_min 
+            return (
+                sigma_min ** (1 / p)
+                + (1-t) * (sigma_max ** (1 / p) - sigma_min ** (1 / p))
+            ) ** p
 
-        # noisy_batch["frame_trans"] = self
-        def model_func(pos, t):
-            noisy_batch["struct_noise"] = torch.ones_like(noisy_batch["struct_noise"]) * t
-            noisy_batch["struct"] = pos
-            _, readout = model.forward(noisy_batch)
-            return readout["trans"]
+        
+        StructureStepper = {
+            'EDMDiffusion': EDMDiffusionStepper,
+            'GaussianFM': GaussianFMStepper,
+        }[self.cfg.struct.type]
+        sampler = OpenProtSampler(schedules={
+            'structure': edm_sched_fn,
+        }, steppers=[
+            StructureStepper(self.cfg.struct)
+        ])
+        
+        sample_batch, extra = sampler.sample(model, noisy_batch, self.cfg.steps)
+        pred_traj = torch.stack(extra['preds'])
+        samp_traj = torch.stack(extra['traj'])
 
-        samp_traj, pred_traj = diffusion.inference(
-            model_func, cfg=self.cfg, mask=noisy_batch["pad_mask"], return_traj=True
-        )
+        B = len(sample_batch['struct'])
+        
+        for i in range(B):
+            prot = make_ca_prot(
+                sample_batch['struct'][i].cpu().numpy(),
+                batch["aatype"][i].cpu().numpy(),
+                batch["struct_mask"][i].cpu().numpy(),
+            )
+    
+            ref_str = protein.to_pdb(prot)
+            name = batch["name"][i]
+            with open(f"{savedir}/{name}.pdb", "w") as f:
+                f.write(ref_str)
+    
+            with open(f"{savedir}/{name}_traj.pdb", "w") as f:
+                f.write(write_ca_traj(prot, samp_traj[:, i].cpu().numpy()))
+    
+            with open(f"{savedir}/{name}_pred_traj.pdb", "w") as f:
+                f.write(write_ca_traj(prot, pred_traj[:, i].cpu().numpy()))
 
-        prot = make_ca_prot(
-            samp_traj[-1, -1].cpu().numpy(),
-            batch["aatype"].cpu().numpy()[0],
-            batch["frame_mask"].cpu().numpy()[0],
-        )
-        name = batch["name"][0]
-        with open(f"{savedir}/{name}_traj.pdb", "w") as f:
-            f.write(write_ca_traj(prot, samp_traj[:, 0].cpu().numpy()))
-
-        with open(f"{savedir}/{name}_pred_traj.pdb", "w") as f:
-            f.write(write_ca_traj(prot, pred_traj[:, 0].cpu().numpy()))
-
-        return samp_traj[-1]
+        return sample_batch['struct']
 
     def run_batch(
         self,
@@ -84,45 +105,50 @@ class StructurePredictionEval(OpenProtEval):
             _, readout = model.forward(noisy_batch)
             coords = readout["trans"][-1]
 
-        L = batch["struct"].shape[1]
-        aatype = batch["aatype"].cpu().numpy()[0]
-
+        
         coords = rmsdalign(batch["struct"], coords, batch["struct_mask"])
-
-        lddt = compute_lddt(coords, batch["struct"], batch["struct_mask"])
-
-        tmscore = compute_tmscore(  # second is reference
-            coords1=coords.cpu().numpy()[0],
-            coords2=batch["struct"].cpu().numpy()[0],
-            seq1=aatype,
-            seq2=aatype,
-            mask1=None,
-            mask2=batch["struct_mask"].cpu().numpy()[0],
-            seq=True
-        )
-
-        rmsd = compute_rmsd(batch["struct"], coords, batch["struct_mask"])
-        if logger:
-            logger.log("struct/lddt", lddt)
-            logger.log("struct/tm", tmscore["tm"])
-            logger.log("struct/gdt_ts", tmscore["gdt_ts"])
-            logger.log("struct/gdt_ha", tmscore["gdt_ha"])
-            logger.log("struct/rmsd", rmsd)
-
-        prot = make_ca_prot(
-            coords=batch["struct"].cpu().numpy()[0],
-            aatype=aatype,
-            mask=batch["struct_mask"].cpu().numpy()[0],
-        )
-        ref_str = protein.to_pdb(prot)
-
-        prot.atom_mask[..., 1] = 1.0
-        prot.atom_positions[..., 1, :] = coords.cpu().numpy()
-        pred_str = protein.to_pdb(prot)
-
-        ref_str = "\n".join(ref_str.split("\n")[1:-3])
-        pred_str = "\n".join(pred_str.split("\n")[1:-3])
-
-        name = batch["name"][0]
-        with open(f"{savedir}/{name}_{lddt.item():.2f}.pdb", "w") as f:
-            f.write("\nENDMDL\nMODEL\n".join([ref_str, pred_str]))
+        B = len(coords)
+        for i in range(B):
+            aatype = batch["aatype"].cpu().numpy()[i]
+            
+            lddt = compute_lddt(
+                coords[i],
+                batch["struct"][i],
+                batch["struct_mask"][i]
+            )
+            
+            tmscore = compute_tmscore(  # second is reference
+                coords1=coords.cpu().numpy()[i],
+                coords2=batch["struct"].cpu().numpy()[i],
+                seq1=aatype,
+                seq2=aatype,
+                mask1=None,
+                mask2=batch["struct_mask"].cpu().numpy()[i],
+                seq=True
+            )
+    
+            rmsd = compute_rmsd(batch["struct"][i], coords[i], batch["struct_mask"][i])
+            if logger:
+                logger.log(f"{self.cfg.name}/lddt", lddt)
+                logger.log(f"{self.cfg.name}/tm", tmscore["tm"])
+                logger.log(f"{self.cfg.name}/gdt_ts", tmscore["gdt_ts"])
+                logger.log(f"{self.cfg.name}/gdt_ha", tmscore["gdt_ha"])
+                logger.log(f"{self.cfg.name}/rmsd", rmsd)
+    
+            prot = make_ca_prot(
+                coords=batch["struct"].cpu().numpy()[i],
+                aatype=aatype,
+                mask=batch["struct_mask"].cpu().numpy()[i],
+            )
+            ref_str = protein.to_pdb(prot)
+            
+            prot.atom_mask[..., 1] = batch["pad_mask"].cpu().numpy()[i]
+            prot.atom_positions[..., 1, :] = coords.cpu().numpy()[i]
+            pred_str = protein.to_pdb(prot)
+    
+            ref_str = "\n".join(ref_str.split("\n")[1:-3])
+            pred_str = "\n".join(pred_str.split("\n")[1:-3])
+    
+            name = batch["name"][i]
+            with open(f"{savedir}/{name}_{lddt.item():.2f}.pdb", "w") as f:
+                f.write("\nENDMDL\nMODEL\n".join([ref_str, pred_str]))
