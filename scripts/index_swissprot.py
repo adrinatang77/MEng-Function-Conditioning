fda89890b23889c33693896059543d9795a43724
aesappington <asappington@hms.harvard.edu>
2025-03-23 18:41:39 -0400

    fix indexing

--- scripts/index_swissprot.py
+++ scripts/index_swissprot.py
@@ -10,7 +10,6 @@ import numpy as np
 import json
 
 # index the file
-# starts = []
 idx = {}
 pbar = tqdm.tqdm()
 
@@ -37,6 +36,22 @@ with open(args.fasta) as f:
                 prev_name = name
             pbar.update()
 
-with open(args.out, 'w') as f:
-    json.dump(idx, f)
+# create idx numpy array 
+starts = []
+prev_start = 0
+uniref = '/data/cb/scratch/datasets/uniref50.fasta'
+with open(uniref) as f: # going in same order as uniref50
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        if line[0] == ">":
+            name = line.split(' ')[0]
+            if name in idx: 
+                starts.append(idx[name]['start']) # add to func idx
+                prev_start = idx[name]['start']
+            else:
+                starts.append(prev_start) # no func label
 
+np.save(args.out, np.array(starts))
+# np.save('/data/cb/scratch/datasets/func_data/extracted_data/swissprot_uniref50/swissprot_GO_idx.npy', np.array(starts))
