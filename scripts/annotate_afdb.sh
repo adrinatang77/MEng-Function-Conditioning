d837ddff7d606fa15f80dcb21ca9e1053ec41ac6
bjing2016 <bjing2016@github.com>
2025-03-25 18:42:53 -0400

    scripts

--- scripts/annotate_afdb.sh
+++ scripts/annotate_afdb.sh
@@ -3,5 +3,6 @@
 # done
 
 for i in {0..71}; do
-     python -m scripts.annotate_foldcomp --db /data/cb/scratch/datasets/afdb_uniref_v4/afdb_uniprot_v4 --out tmp/$i.pkl --num_workers 72 --worker_id $i & 
+     python -m scripts.annotate_foldcomp --db /data/cb/scratch/datasets/afdb_uniref_v4/afdb_uniprot_v4 --out tmp/afdb_uniprot_v4.idx.$i --num_workers 72 --worker_id $i & 
 done
+wait
--- scripts/annotate_foldcomp.py
+++ scripts/annotate_foldcomp.py
@@ -1,8 +1,8 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--db", type=str, default=None)
-parser.add_argument("--out", type=str, default=None)
+parser.add_argument("--db", type=str, default=None, required=True)
+parser.add_argument("--out", type=str, default=None, required=True)
 parser.add_argument("--num_workers", type=int, default=1)
 parser.add_argument("--worker_id", type=int, default=0)
 
@@ -17,19 +17,12 @@ from openprot.utils.prot_utils import aatype_to_seqres
 db = foldcomp.open(args.db)
 
 df = []
-with open(f"{args.out}.fasta", "w") as f:
+with open(args.out, "w") as f:
     for i in tqdm.trange(args.worker_id, len(db), args.num_workers):
         name, pdb = db[i]
         prot = protein.from_pdb_string(pdb)
-        seqres = aatype_to_seqres(prot.aatype)
-        f.write(f">{name}\n{seqres}\n")
-        df.append(
-            {
-                "index": i,
-                "name": name,
-                "plddt": prot.b_factors[:, 1].mean(),
-                "length": len(prot.aatype),
-            }
-        )
+        f.write(f"{i} {name} {round(prot.b_factors[:, 1].mean())} {len(prot.aatype)}\n")
+        
+# pd.DataFrame(df).set_index("index").to_pickle(args.out)
+# with open(f"{args.out}.fasta", "w") as f:
 
-pd.DataFrame(df).set_index("index").to_pickle(args.out)
